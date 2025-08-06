61c2fa40172907973630347afe0f2ee18f75245a
AvenkyrPakacit <pwthesnell2017@gmail.com>
2025-08-05 20:11:30 -0500

    Add DLL, as a attempted step toward biome generation
    Barely know how to code.

---
+++ Source/RimAvenskol_SourceFiles/BiomeDef.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RimWorld
+{
+    // Token: 0x020035A1 RID: 13729
+    [DefOf]
+    public static class BiomeDefOf
+    {
+        // Token: 0x9991CEDE RID: 99991
+        public static BiomeDef Glowforest;
+    }
+}
